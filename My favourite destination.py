29c52f45b5db500a39a1fc4628fbe67b4144dc9c
MpiPuin12 <96949872+MpiPuin12@users.noreply.github.com>
2022-01-17 18:12:52 +0100

    Add files via upload

---
+++ My favourite destination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Purpose of the notebook: The purpose of this notebook is for me to explain what are my ideal vacations and trying to give some details about this ideal place.
+
+# ## Photo of the destination ![palafrugell-girona.jpeg](attachment:palafrugell-girona.jpeg)
+
+# ## Video of the destination https://www.youtube.com/watch?v=EHnkcaw9n1U
+
+# # Define Function
+
+# In[1]:
+
+
+def function (name):
+    print("My name is "+ name)
+function ('Marc')
+
+
+# # Insert photo of myself
+# ![Captura%20de%20pantalla%202021-11-08%20a%20las%2011.39.47.png](attachment:Captura%20de%20pantalla%202021-11-08%20a%20las%2011.39.47.png)
+
+# #  What's your prefered destination
+
+# In[ ]:
+
+
+#I've just asked for his ideal destination.
+
+
+X = int(input("Press 1 if you prefer a beach destination or Press 2 if you prefer a mountain destination"))
+#y = input("What is you favourite destination?")
+z = float(X)
+
+if z == 1:
+    y=('Beach')
+    print(y)
+elif z == 2:
+    y=('Mountain')
+    print(y)
+else:
+    print("Please enter either 1 or 2")
+
+
+# # _**My top 3 favourite things to do at this destination are:**_
+# 1. Go to the beach
+# 2. Play Golf
+# 3. Party with my friends
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
